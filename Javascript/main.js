95fa9bfa75d0239f063a8b8332044198dbaacf7d
Yanis <yhamdy.pro@outlook.fr>
2022-12-06 19:55:18 +0100

    06/12/2022

---
+++ Javascript/main.js
@@ -0,0 +1,39 @@
+var load_screen = document.getElementById("load_screen");
+var arrow = document.getElementById("arrow_down");
+
+
+
+
+
+setTimeout(() => {
+    load_screen.style.visibility = 'hidden';
+}, 3000);
+
+var arrowToDown = anime({
+    targets: '#arrow_down',
+    translateY: 50,
+    duration: 1000,
+    autoplay: false
+});
+
+var arrowToUp = anime({
+    targets: '#arrow_down',
+    translateY: -25,
+    duration: 1000,
+    autoplay: false
+});
+
+arrow.addEventListener('mouseover', () => {
+    arrowToDown.play();
+    setTimeout(() => {
+        arrowToUp.play();
+    },2000);
+});
+arrow.addEventListener('click', event => {
+    $(".main").moveDown();
+})
+
+
+$(".main").onepage_scroll({
+    pagination: false
+});
